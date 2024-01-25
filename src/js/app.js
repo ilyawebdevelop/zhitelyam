0b27535748cf9732919c86e0ecb9cf96b7b0dfc2
ilyawebdevelop <ilia966@tut.by>
2024-01-25 09:59:33 +0300

    123

--- src/js/app.js
+++ src/js/app.js
@@ -13,69 +13,96 @@ Swiper.use([Navigation, Pagination, Autoplay, Mousewheel, EffectFade, Thumbs, Sc
 
 window.onload = function () {
 
-  new fullpage('#fullpage', {
+  var myFullpage = new fullpage('#fullpage', {
     anchors: ['slide1', 'slide2', 'slide3', 'slide4', 'slide5', 'slide6', 'slide7'],
     menu: '.menu',
     autoScrolling: true,
     scrollHorizontally: true,
     fitToSection: false,
-    navigation: false,   
+    navigation: false,
     // responsiveWidth: 768
+    beforeLeave: function (origin) {
+      console.log(origin);
+    },
   });
+
+
+  // Инициализация слайдера content-slider
+  document.querySelectorAll('.content-slider').forEach(n => {
+    const mySwiperContent = new Swiper(n, {
+      slidesPerView: 1,
+      spaceBetween: 20,
+      speed: 600,
+      effect: 'fade',
+      mousewheelControl: true,
+      watchOverflow: true,
+      mousewheel: {
+        invert: false,
+      },
+      fadeEffect: {
+        crossFade: true
+      },
+      autoHeight: true,
+      on: {
+        slideChange: function () {
+          var slides_count = n.querySelectorAll(".swiper-slide"); // <- add this
+          let offer = n.closest('.section').querySelector('.counter');
+          offer.innerHTML = '0' + (this.activeIndex + 1) + '' + `<span> /` + ' 0' + (slides_count.length) + '</span>';
+          
+          console.log(this.activeIndex);
+          myFullpage.setAllowScrolling(false);
+        },
+        slideNextTransitionEnd: function(){
+          console.log('123');
+          myFullpage.setAllowScrolling(true);
+        },
+        slidePrevTransitionEnd: function(){
+          console.log('123');
+          myFullpage.setAllowScrolling(true);
+        },
+      },
+      navigation: {
+        nextEl: n.closest('.section').querySelector('.navArrowNext'),
+        prevEl: n.closest('.section').querySelector('.navArrowPrev'),
+      },
+      pagination: {
+        el: n.querySelector('.pagination'),
+        clickable: true,
+        type: 'bullets',
+      },
+      breakpoints: {
+        0: {
+          spaceBetween: 8,
+        },
+        576: {
+          spaceBetween: 20,
+        },
+      },
+    });
+
+
+    function change() {
+
+      setTimeout(function () {
+        var slides_count = n.querySelectorAll(".swiper-slide"); // <- add this
+        let offer_1 = n.closest('.section').querySelector('.counter');
+        if (offer_1) {
+          offer_1.innerHTML = '0' + (mySwiperContent.activeIndex + 1) + '' + `<span> /` + ' 0' + (slides_count.length) + '</span>';
+        }
+      }, 1000);
+    }
+    change();
+  });
+
+
 }
 
 
 
-// Инициализация слайдера content-slider
-document.querySelectorAll('.content-slider').forEach(n => {
-  const mySwiperContent = new Swiper(n, {
-    slidesPerView: 1,
-    spaceBetween: 20,
-    speed: 600,
-    effect: 'fade',
-    fadeEffect: {
-      crossFade: true
-    },
-    autoHeight: true,
-    on: {
-      slideChange: function () {
-        var slides_count = n.querySelectorAll(".swiper-slide"); // <- add this
-        let offer = n.closest('.section').querySelector('.counter');
-        offer.innerHTML = '0' + (this.activeIndex + 1) + '' + `<span> /` + ' 0' + (slides_count.length) + '</span>';
-      }
-    },
-    navigation: {
-      nextEl: n.closest('.section').querySelector('.navArrowNext'),
-      prevEl: n.closest('.section').querySelector('.navArrowPrev'),
-    },
-    pagination: {
-      el: n.querySelector('.pagination'),
-      clickable: true,
-      type: 'bullets',
-    },
-    breakpoints: {
-      0: {
-        spaceBetween: 8,
-      },
-      576: {
-        spaceBetween: 20,
-      },
-    },
-  });
 
 
-  function change() {
 
-    setTimeout(function () {
-      var slides_count = n.querySelectorAll(".swiper-slide"); // <- add this
-      let offer_1 = n.closest('.section').querySelector('.counter');
-      if (offer_1) {
-        offer_1.innerHTML = '0' + (mySwiperContent.activeIndex + 1) + '' + `<span> /` + ' 0' + (slides_count.length) + '</span>';
-      }
-    }, 1000);
-  }
-  change();
-});
+
 
 let introCatalogBtn = document.querySelector('.hamburger');
 let introCatalog = document.querySelector('.introCatalog');
@@ -99,8 +126,8 @@ document.addEventListener('click', function (e) {
 
   if (!its_introCatalogBtn && !its_CatalogContent) {
     introCatalog.classList.remove('active');
-      introCatalogBtn.classList.remove('active');
-      bodyEl.classList.remove('hidden');
+    introCatalogBtn.classList.remove('active');
+    bodyEl.classList.remove('hidden');
   }
 });
 
